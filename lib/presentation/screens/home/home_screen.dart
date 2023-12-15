fce0738881b7f23ba110d8783e23641c97bb8aa5
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2023-12-15 05:16:17 -0700

    go router routing

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -49,8 +50,7 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.of(context).push(
         //   MaterialPageRoute(builder: (context) => ButtonsScreen())
         // )
-
-        Navigator.pushNamed(context, menuItem.link)
+        context.push(menuItem.link)
       },
     );
   }
